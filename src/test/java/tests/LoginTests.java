0f9584756266e683621d23bd230a409f41a4e7aa
Zarina <zarinakargaeva@amins-MBP.localdomain>
2019-02-08 20:37:00 -0500

    Add map object

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -12,6 +12,8 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTests {
@@ -20,6 +22,7 @@ public class LoginTests {
     //without Page object approach
 
         WebDriver driver;
+        Map <String,String> myMap = new HashMap<String, String>();
 
         @BeforeMethod
         public void setUp() {
